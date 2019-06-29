ba1acb64af0883e7c3599f042999a9cc79004b11
Tra My Nguyen <46087525+Chiffon-Nguyen@users.noreply.github.com>
2019-06-29 21:58:26 +0700

    Updated

--- booking.js
+++ booking.js
@@ -1,10 +1,62 @@
-// console.log("K")
-var departureAirport = document.getElementById("departure-airport")
-console.log(departureAirport)
-function getdata(departureAirport, arrivalAirport){
-    for(let i=0 ; i< vemotchieu.length;i++) {
-        if (vemotchieu[i]["d-place"] == departureAirport && vemotchieu[i]["a-place"] == arrivalAirport){
-            console.log(vemotchieu[i])
+function getdata() {
+    var depAirport = document.getElementById("departure-airport");
+    depUser = depAirport.options[depAirport.selectedIndex].text;
+
+    var arrAirport = document.getElementById("arrival-airport");
+    arrUser = arrAirport.options[arrAirport.selectedIndex].text;
+
+    var baby = document.getElementById("baby").value;
+    var child = document.getElementById("child").value;
+    var adult = document.getElementById("adult").value;
+
+    var today = new Date();
+    console.log(today)
+    // if ($("#datepicker").datepicker().val() ="") {
+    //     // check xem user đã chọn ngày chưa
+    //     document.getElementById("date-check").innerHTML = "Bạn chưa chọn ngày!";
+    // }
+    // else if ($("#datepicker").datepicker("getDate") - today < 0) { 
+    //     //check xem user có chọn mấy ngày trong quá khứ không
+    //     document.getElementById("date-check").innerHTML = "Bạn phải chọn ngày "
+    // }
+    // else if (){
+    //     //dành cho vé khứ hồi: check xem hiệu ngày đi - ngày về có lớn hơn 0 không
+    //     document.getElementById("date-check").innerHTML = 
+    // }
+    // else {
+    //     var departureDate = $("#datepicker").datepicker({ dateFormat: 'dd,mm,yyyy' }).val();
+    // }
+
+
+    if (document.getElementById("oneway").checked == true) {
+        for (let i = 0; i < onewayticket.length; i++) {
+            if (onewayticket[i]["d-place"] == depUser && onewayticket[i]["a-place"] == arrUser) {
+                console.log(onewayticket[i])
+                for (let k = 0; k < onewayticket[k]["d-time"]; k++) {
+                    //show time frame here
+                }
+                var price_baby = baby * onewayticket[i]["price"]["baby"];
+                var price_child = child * onewayticket[i]["price"]["child"];
+                var price_adult = adult * onewayticket[i]["price"]["adult"];
+                var total_price = price_child + price_baby + price_adult;
+                console.log(total_price)
+            }
         }
+    }
+    else if (document.getElementById("roundtrip").checked == true) {
+        for (let i = 0; i < onewayticket.length; i++) {
+            if (onewayticket[i]["d-place"] == depUser && onewayticket[i]["a-place"] == arrUser) {
+                for (let k = 0; k < onewayticket[k]["d-time"]; k++) {
+                    //show time frame here
+                }
+                var price_baby = 1.8 * baby * onewayticket[i]["price"]["baby"];
+                var price_child = 1.8 * child * onewayticket[i]["price"]["child"];
+                var price_adult = 1.8 * adult * onewayticket[i]["price"]["adult"];
+                var total_price = price_child + price_baby + price_adult;
+            }
+        }
+    }
+    else {
+        document.getElementById("ticket-check").innerHTML = "You have to choose the ticket type";
     }
 }
--- bookingdata.js
+++ bookingdata.js
@@ -1,80 +1,98 @@
-var vemotchieu = [
+var onewayticket = [
   {"d-place": "Hà Nội",
   "a-place": "Hồ Chí Minh",
   "price": {
     "baby": 1200000,
     "child": 1800000,
     "adult": 2015000, },
-  "d-time": {  //thời gian khởi hành 
-  },
-  "length": //độ dài chuyến bay
+  "d-time": [ "14h - 16h15", "16h30 - 18h45", "18h - 20h15" ],
+  "length": "2h15"
 }, {"d-place": "Hà Nội",
   "a-place": "Đà Lạt",
   "price": {
     "baby": 900000,
     "child": 1500000,
-    "adult": 1800000, }
+    "adult": 1800000, } ,
+    "d-time": [ "12h - 13h45", "16h - 17h45", "20h - 21h45" ],
+    "length": "1h45"
 }, {"d-place": "Hà Nội",
   "a-place": "Đà Nẵng",
-  "price": {
-    "baby": 1300000,
-    "child": 1800000,
-    "adult": 2000000, }
+  "price": { "baby": 1300000, "child": 1800000, "adult": 2000000, }, 
+  "d-time": [ "13h - 14h55", "17h - 18h55", "21h - 22h55" ],
+  "length": "1h55"
 }, {"d-place": "Hà Nội",
   "a-place": "Hải Phòng",
   "price": {
     "baby": 500000,
     "child": 800000,
-    "adult": 1000000, }
+    "adult": 1000000, }, 
+  "d-time": [ "10h - 11h20", "15h - 16h20", "22h - 23h20" ],
+  "length": "1h20"
 }, {"d-place": "Hà Nội",
   "a-place": "Nha Trang",
   "price": {
     "baby": 1600000,
     "child": 2000000,
-    "adult": 2200000, }
+    "adult": 2200000, },
+  "d-time": [ "9h - 11h10", "18h - 20h10", "20h - 22h10" ],
+  "length": "2h10"
 } , {"d-place": "Hà Nội",
   "a-place": "Phú Quốc",
   "price": {
     "baby": 1815000,
     "child": 2100000,
-    "adult": 2250000, } //hết Hà Nội
-}, {"d-place": "Hồ Chí Minh",
+    "adult": 2250000, }, 
+  "d-time": [ "13h - 15h35", "16h10 - 18h45", "21h20 - 23h55" ],
+  "length": "2h35" 
+}, {"d-place": "Hồ Chí Minh", //HCM
   "a-place": "Hà Nội",
   "price": {
     "baby": 1200000,
     "child": 1800000,
     "adult": 2015000, },
+  "d-time": [ "14h - 16h15", "16h30 - 18h45", "18h - 20h15" ],
+  "length": "2h15"
 }, {"d-place": "Hồ Chí Minh",
   "a-place": "Đà Lạt",
   "price": {
     "baby": 1300000,
     "child": 1570000,
-    "adult": 1820000, }
+    "adult": 1820000, },
+  "d-time": [ "12h15 - 13h55", "16h30 - 18h10", "19h30 - 21h10" ],
+  "length": "1h40"
 }, {"d-place": "Hồ Chí Minh",
   "a-place": "Đà Nẵng",
   "price": {
     "baby": 1300000,
     "child": 1800000,
-    "adult": 2000000, }
+    "adult": 2000000, }, 
+  "d-time": [ "12h30 - 14h10", "17h20 - 19h", "20h45 - 22h25" ],
+  "length": "1h40"
 }, {"d-place": "Hồ Chí Minh",
   "a-place": "Hải Phòng",
   "price": {
     "baby": 1800000,
     "child": 1990000,
-    "adult": 2150000, }
+    "adult": 2150000, },
+  "d-time": [ "14h - 16h25", "16h30 - 18h55", "18h - 20h25" ],
+  "length": "2h25"
 }, {"d-place": "Hồ Chí Minh",
   "a-place": "Nha Trang",
   "price": {
     "baby": 1550000,
     "child": 1800000,
-    "adult": 2000000, }
+    "adult": 2000000, },
+  "d-time": [ "15h - 16h45", "18h30 - 20h15", "21h10 - 22h55" ],
+  "length": "1h45"
 } , {"d-place": "Hồ Chí Minh",
   "a-place": "Phú Quốc",
   "price": {
     "baby": 1815000,
     "child": 1900000,
-    "adult": 2000000, } //hết HCM
-}, {"d-place": "Đà Lạt",
+    "adult": 2000000, },
+  "d-time": [ "11h - 12h20", "15h30 - 16h50", "18h - 19h20" ],
+  "length": "1h20"
+}, {"d-place": "Đà Lạt", // Đà lạt
   "a-place": "Hà Nội",
   "price": {
     "baby": 900000,
@@ -109,8 +127,8 @@ var vemotchieu = [
   "price": {
     "baby": 1515000,
     "child": 1700000,
-    "adult": 1900000, } //hết Đà Lạt
-}, {"d-place": "Đà Nẵng",
+    "adult": 1900000, } 
+}, {"d-place": "Đà Nẵng", //Đà Nẵng
   "a-place": "Hà Nội",
   "price": {
     "baby": 1300000,
@@ -255,3 +273,4 @@ var vemotchieu = [
     "child": 1620000,
     "adult": 1700000, } //hết Phú Quốc 
 }]
+var vekhuhoi 
